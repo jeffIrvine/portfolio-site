b284b363ff7175275b6f594b0f04d1b61a6b5d95
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-12-29 16:54:17 -0800

    messing with material ui buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import ButtonGroup from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+
 
 function App() {
   return (
     <div className="App">
           Hello World!
+          <ButtonGroup>
+          <Button
+          startIcon={<SaveIcon />} 
+          variant="contained"
+          color="primary"
+          >
+            Click Me!
+          </Button>
+          <Button
+          startIcon={<SaveIcon />} 
+          variant="contained"
+          color="primary"
+          >
+            Don't Click
+          </Button>
+          </ButtonGroup>
     </div>
   );
 }
