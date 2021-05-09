be270a4a96dcb2de044dcc854b06aa74823ebd41
jeffIrvine <jirvine1212@gmail.com>
2021-05-09 10:57:12 -0700

    pdf viewer testing

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -9,15 +9,18 @@ const Resume = (props) => {
   const { pdf } = props;
 
   return (
-    <Document
-      file={pdf}
-      options={{ workerSrc: "/pdf.worker.js" }}
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
-      {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index +1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
+    <section>
+      <h1>Resume</h1>
+      <div className="pdf-doc">
+        <Document
+          file={pdf}
+          options={{ workerSrc: "/pdf.worker.js" }}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={numPages} />
+        </Document>
+      </div>
+    </section>
   )
 }
 
