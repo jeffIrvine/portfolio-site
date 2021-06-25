54c433325c8ffe8281557539ee693e849ebd312c
jeffIrvine <jirvine1212@gmail.com>
2021-06-25 16:54:06 -0700

    updated resume pdf

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,11 @@ import Header from '../components/header/Header';
 import About from '../components/about/About';
 import TechStack from '../components/techStack/TechStack';
 import Resume from '../components/resume/Resume';
-import jeffIrvineResume from '../images/JeffIrvineResume1.7.pdf';
+import jeffIrvineResume from '../images/JeffIrvineResume2.1.pdf';
 import './App.css'
 
 const App = () => {
-  const [renderComp, setComp] = useState('testH1');
+  const [renderComp, setComp] = useState(null);
   
   const navMatrix = {
     aboutMe: <About />,
