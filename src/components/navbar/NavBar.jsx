e89c874498cae9c20ac1639b861e29e7118970a0
Jeff Irvine (he/him) <68131101+jeffIrvine@users.noreply.github.com>
2023-05-17 07:35:13 -0700

    Removing resume viewer

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import './NavBar.css'
+import React from 'react';
+import './NavBar.css';
 
 const NavBar = ({ setComp }) => {
-
   const onClick = ({ target }) => {
-    setComp(target.id)
-  }
+    setComp(target.id);
+  };
   return (
     <>
-    <div className="nav-container">
-      <div className="name-title">
-        <h1>Jeff Irvine</h1>
-        <h1>Software Engineer</h1>
+      <div className='nav-container'>
+        <div className='name-title'>
+          <h1>Jeff Irvine</h1>
+          <h1>Software Engineer</h1>
+        </div>
+        <div className='nav-buttons'>
+          <button id='techStack' className='nav-links' onClick={onClick}>
+            Tech Stack
+          </button>
+          <button id='aboutMe' className='nav-links' onClick={onClick}>
+            About Me
+          </button>
+          <button id='projects' className='nav-links' onClick={onClick}>
+            Projects
+          </button>
+        </div>
       </div>
-      <div className="nav-buttons">
-        <button id="techStack" className="nav-links" onClick={onClick} >Tech Stack</button>
-        <button id="aboutMe" className="nav-links" onClick={onClick} >About Me</button>
-        <button id="projects" className="nav-links" onClick={onClick} >Projects</button>
-        <button id="Resume" className="nav-links" onClick={onClick} >Resume</button>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
